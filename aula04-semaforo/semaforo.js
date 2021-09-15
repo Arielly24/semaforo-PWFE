5b1a0a55c86b60bb790459e679daf4977ad675d5
AriellyAngelo <79976817+Arielly24@users.noreply.github.com>
2021-09-15 11:26:04 -0300

    Add files via upload

--- aula04-semaforo/semaforo.js
+++ aula04-semaforo/semaforo.js
@@ -1,3 +1,33 @@
 "use strict"
 
+const img = document.getElementById( 'img' );
+const buttons = document.getElementById( 'buttons' );
+let corIndex = 0;
+let intervalId = null;
+
+const mudançaCor = ( event ) => {
+    parar();
+    ligar[event.target.id]();
+}
+
+const nextIndex = () => corIndex = corIndex < 2 ? ++corIndex : 0;
+
+const mudarCor = () => {
+    const cores = ['pare','siga','desacelere']
+    const cor = cores[ corIndex ];
+    ligar[cor]();
+    nextIndex();
+}
+
+const parar = () => {
+    clearInterval ( intervalId );
+}
+
+const ligar = {
+    'pare':() => img.src = './img/vermelho.png',
+    'desacelere':() => img.src = './img/amarelo.png',
+    'siga':() => img.src = './img/verde.png',
+    'automatico':() => intervalId = setInterval( mudarCor, 100 )
+}
+
 
